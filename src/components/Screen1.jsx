2aeb4068819788cc338bf94208908613bfd720a1
jacquikeating <j.keating@live.ca>
2026-02-03 13:53:48 -0500

    Screen1 options call setCardsOrInters on click

--- src/components/Screen1.jsx
+++ src/components/Screen1.jsx
@@ -1,10 +1,10 @@
-export default function Screen1() {
+export default function Screen1({ setCardsOrInters }) {
     return (
         <div className="screen-1">
-            <div className="option">
+            <div className="option" onClick={() => setCardsOrInters("cards")}>
                 <span className="option-name">Cards</span>
             </div>
-            <div className="option">
+            <div className="option" onClick={() => setCardsOrInters("intercards")}>
                 <span className="option-name">Intercards</span>
             </div>
         </div>
