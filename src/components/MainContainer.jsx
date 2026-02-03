60559b4f4d017798ffc3b6e20435e9f2679362c6
jacquikeating <j.keating@live.ca>
2026-02-03 13:47:47 -0500

    Created Screen1 component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,12 @@
 import MenuBar from "./MenuBar"
+import Screen1 from "./Screen1"
 
 export default function MainContainer() {
     return (
         <div className="container">
             <MenuBar />
             <main>
+                <Screen1 />
             </main>
         </div>
     )
---
+++ src/components/Screen1.jsx
@@ -0,0 +1,12 @@
+export default function Screen1() {
+    return (
+        <div className="screen-1">
+            <div className="option">
+                <span className="option-name">Cards</span>
+            </div>
+            <div className="option">
+                <span className="option-name">Intercards</span>
+            </div>
+        </div>
+    )
+}
