0f0c08450b3d69e5113eec97fae55466db7e6dc0
jacquikeating <j.keating@live.ca>
2026-02-03 15:16:39 -0500

    Replaced hardcoded job buttons with a .map() using jobOptions

--- src/components/Screen2.jsx
+++ src/components/Screen2.jsx
@@ -38,34 +38,29 @@ export default function Screen2() {
             mechanic: "def 2",
             group: 2
         },
+        {
+            location: "N",
+            quadrant: 1,
+            mechanic: "stack",
+            group: 1
+        },
+        {
+            location: "NE",
+            quadrant: 2,
+            mechanic: "stack",
+            group: 1
+        },
     ]
 
     return (
         <div className="screen-2">
-            <button class="clone-button" style={{"--i": 0}}>
-                <span>E</span>
-            </button>
-            <button class="clone-button" style={{"--i": 1}}>
-                <span>SE</span>
-            </button>
-            <button class="clone-button" style={{"--i": 2}}>
-                <span>S</span>
-            </button>
-            <button class="clone-button" style={{"--i": 3}}>
-                <span>SW</span>
-            </button>
-            <button class="clone-button" style={{"--i": 4}}>
-                <span>W</span>
-            </button>
-            <button class="clone-button" style={{"--i": 5}}>
-                <span>NW</span>
-            </button>
-            <button class="clone-button" style={{"--i": 6}}>
-                <span>N</span>
-            </button>
-            <button class="clone-button" style={{"--i": 7}}>
-                <span>NE</span>
-            </button>
+            {jobOptions.map((job, index) => {
+                return (
+                    <button className="clone-button" style={{"--i": index}} key={index}>
+                        {job.location}
+                    </button>
+                )
+            })}
         </div>
     )
 }
