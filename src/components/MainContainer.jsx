b8de6c8a526a3916fc7836b4a7a7e3e51ef93616
jacquikeating <j.keating@live.ca>
2026-02-03 13:53:28 -0500

    Set up cardsOrInters state

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,16 @@
 import MenuBar from "./MenuBar"
 import Screen1 from "./Screen1"
+import { useState } from "react"
 
 export default function MainContainer() {
+    const [cardsOrInters, setCardsOrInters] = useState(null)
+
     return (
         <div className="container">
             <MenuBar />
+            <p>{cardsOrInters}</p>
             <main>
-                <Screen1 />
+                <Screen1 setCardsOrInters={setCardsOrInters} />
             </main>
         </div>
     )
