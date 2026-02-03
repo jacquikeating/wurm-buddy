1f48f6e528e0dee8ce9451854adc8db4fca0a632
jacquikeating <j.keating@live.ca>
2026-02-03 15:22:40 -0500

    Created myJob state

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,18 +5,20 @@ import { useState } from "react"
 
 export default function MainContainer() {
     const [cardsOrInters, setCardsOrInters] = useState(null)
+    const [myJob, setMyJob] = useState(null)
 
     return (
         <div className="container">
             <MenuBar />
             <div className="stored-variables">
                 {cardsOrInters && <p>{cardsOrInters}</p>}
+                {myJob && <p>{myJob.mechanic}</p>}
             </div>
             <main>
                 {!cardsOrInters ? (
                     <Screen1 setCardsOrInters={setCardsOrInters} />
                 ) : (
-                    <Screen2 />
+                    <Screen2 setMyJob={setMyJob} />
                 )}
                 
             </main>
