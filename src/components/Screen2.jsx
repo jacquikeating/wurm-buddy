d9b3f88f59e0623098b692a73cafa00e642a546c
jacquikeating <j.keating@live.ca>
2026-02-03 14:57:46 -0500

    Created 8 buttons in a circle oin Screen2

--- src/components/Screen2.jsx
+++ src/components/Screen2.jsx
@@ -1,7 +1,30 @@
 export default function Screen2() {
     return (
-        <div>
-            <h2>2</h2>
+        <div className="screen-2">
+            <button class="clone-button" style={{"--i": 0}}>
+                <span>E</span>
+            </button>
+            <button class="clone-button" style={{"--i": 1}}>
+                <span>SE</span>
+            </button>
+            <button class="clone-button" style={{"--i": 2}}>
+                <span>S</span>
+            </button>
+            <button class="clone-button" style={{"--i": 3}}>
+                <span>SW</span>
+            </button>
+            <button class="clone-button" style={{"--i": 4}}>
+                <span>W</span>
+            </button>
+            <button class="clone-button" style={{"--i": 5}}>
+                <span>NW</span>
+            </button>
+            <button class="clone-button" style={{"--i": 6}}>
+                <span>N</span>
+            </button>
+            <button class="clone-button" style={{"--i": 7}}>
+                <span>NE</span>
+            </button>
         </div>
     )
 }
