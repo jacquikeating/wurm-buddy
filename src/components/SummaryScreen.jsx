b2fd7e2cf152fe7cd62caad23394238ade19b187
jacquikeating <j.keating@live.ca>
2026-02-12 13:04:27 -0500

    Styled SummaryScreen

--- src/components/SummaryScreen.jsx
+++ src/components/SummaryScreen.jsx
@@ -3,7 +3,7 @@ export default function SummaryScreen({ summary }) {
     const {quadrant, hourglass, tetherType, firstMech, instructions, tower, cardsOrInters, safePlatform, portalClone} = summary
 
     return (
-        <>
+        <div className="summary-screen">
             {/* <div className="stored-variables">
                     {cardsOrInters && <p>{cardsOrInters}</p>}
                     {myJob && (<p>take {myJob.mechanic} tether in quadrant {myJob.quadrant}</p>)}
@@ -15,25 +15,25 @@ export default function SummaryScreen({ summary }) {
 
             <div className="mech-group">
                 <h2>Start</h2>
-                <p>{quadrant && <span>{quadrant || "?"} quad, {tetherType} tether</span>}</p>
-                <p>{hourglass && <span>{hourglass} safe</span>}</p>
+                <p>{quadrant && <span>• {quadrant || "?"} quad, {tetherType} tether</span>}</p>
+                <p>{hourglass && <span>• {hourglass} safe</span>}</p>
             </div>
 
             <div className="mech-group">
                 <h2>Mid</h2>
-                <p>{firstMech && <span>{firstMech} first</span>}</p>
-                <p>{summary.instructions && <span>{summary.instructions[0]}</span>}</p>
-                <p>{tower && <span>back {tower[0]} tower ({tower[1]})</span>}</p>
+                <p>{firstMech && <span>• {firstMech} first</span>}</p>
+                <p>{summary.instructions && <span>• {summary.instructions[0]}</span>}</p>
+                <p>{tower && <span>• back {tower[0]} tower ({tower[1]})</span>}</p>
             </div>
 
             <div className="mech-group">
                 <h2>End</h2>
-                <p>{cardsOrInters && <span>stack on {cardsOrInters}</span>}</p>
-                <p>{safePlatform && <span>{safePlatform}</span>}</p>
-                <p>{cardsOrInters && <span>stack on {cardsOrInters == "cards" ? "intercards" : "cards" }</span>}</p>
-                <p>{portalClone && <span>{portalClone}</span>}</p>
+                <p>{cardsOrInters && <span>• stack on {cardsOrInters}</span>}</p>
+                <p>{safePlatform && <span>• {safePlatform}</span>}</p>
+                <p>{cardsOrInters && <span>• stack on {cardsOrInters == "cards" ? "intercards" : "cards" }</span>}</p>
+                <p>{portalClone && <span>• {portalClone}</span>}</p>
             </div>
-        </>
+        </div>
         
     )
 }
