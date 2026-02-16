d7f76807467d2deb88ce3f52ab5bbe3a9226470b
jacquikeating <j.keating@live.ca>
2026-02-16 14:23:30 -0500

    Added handleInput and setStep to Screen7

--- src/components/Screen7.jsx
+++ src/components/Screen7.jsx
@@ -1,13 +1,18 @@
 export default function Screen7({ portalClone, setSafePlatform }) {
 
+    function handleInput(selectedOption) {
+        setSafePlatform(selectedOption)
+        setStep(10)
+    }
+
     let safeSide = portalClone == "sides safe" ? ("north safe") : ("boss safe")
 
     return (
         <div className="screen-7 side-split">
-            <div className="option" onClick={() => setSafePlatform(`west platform, ${safeSide}`)}>
+            <div className="option" onClick={() => handleInput(`west platform, ${safeSide}`)}>
                 <span className="option-name">West</span>
             </div>
-            <div className="option" onClick={() => setSafePlatform(`east platform, ${safeSide}`)}>
+            <div className="option" onClick={() => handleInput(`east platform, ${safeSide}`)}>
                 <span className="option-name">East</span>
             </div>            
         </div>
