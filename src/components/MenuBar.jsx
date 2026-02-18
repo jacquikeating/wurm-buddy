d78ba7482f15b9a6a0b995a76c7eec54e1930218
jacquikeating <j.keating@live.ca>
2026-02-18 09:14:46 +0700

    Added a reset button to MenuBar

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,7 +1,13 @@
 export default function MenuBar({ uptime, setUptime }) {
+
+    function reset() {
+        console.log("reset")
+    }
+
     return (
         <div className="menu-bar">
             <button className="toggle-button" onClick={() => setUptime(!uptime)}>{ uptime ? "uptime" : "regular"}</button>
+            <button className="toggle-button" onClick={reset}>reset</button>
         </div>
     )
 }
