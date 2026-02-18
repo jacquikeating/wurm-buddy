2e7bfa5703dc421de1f1f730f217c9b68666cb50
jacquikeating <j.keating@live.ca>
2026-02-19 09:15:00 +1400

    Added pref-group <divs> to Preferences

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -2,9 +2,21 @@ export default function Preferences({ prefsOpen, uptime, setUptime }) {
 
     return (
         <div className={prefsOpen ? ("prefs open") : ("prefs")}>
-            <button className={uptime ? "settings-button" : "settings-button active"} onClick={() => setUptime(false)}>regular</button>
-            <button className={uptime ? "settings-button active" : "settings-button"} onClick={() => setUptime(true)}>uptime</button>
-            <p>{uptime ? ("uptime") : ("regular")}</p>
+            <div className="pref-group">
+                <p>strat:</p>
+                <button className={uptime ? "settings-button" : "settings-button active"} onClick={() => setUptime(false)}>regular</button>
+                <button className={uptime ? "settings-button active" : "settings-button"} onClick={() => setUptime(true)}>uptime</button>
+                <p>{uptime ? ("uptime") : ("regular")}</p>
+            </div>
+
+            <div className="pref-group">
+                <p>size:</p>
+            </div>
+
+            <div className="pref-group">
+                <p>role:</p>
+            </div>
+            
         </div>
     )
 }
