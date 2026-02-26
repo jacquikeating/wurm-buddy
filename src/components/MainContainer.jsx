d7168b79bc18328b739d583aa24966412a8a2a47
jacquikeating <j.keating@live.ca>
2026-02-27 11:17:38 +1400

    Preferences stored in localStorage

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -45,10 +45,16 @@ export default function MainContainer() {
     const setMechanicVariables = [setStep, setCardsOrInters, setMyJob, setHourglassLocation, setFirstMech, setTower, setPortalClone, setSafePlatform]
 
     // PREFERENCES
+    const localStoragePrefs = JSON.parse(localStorage.getItem("prefs"))
+    let defaultPrefs = {uptime: true, size: "mini", role: "GenericMelee"}
+    if (localStoragePrefs) {
+        defaultPrefs = localStoragePrefs
+    }
+
     const [prefsOpen, setPrefsOpen] = useState(false)
-    const [uptime, setUptime] = useState(true)
-    const [size, setSize] = useState("mini")
-    const [role, setRole] = useState("Generic")
+    const [uptime, setUptime] = useState(defaultPrefs.uptime)
+    const [size, setSize] = useState(defaultPrefs.size)
+    const [role, setRole] = useState(defaultPrefs.role)
 
     const prefs = [prefsOpen, setPrefsOpen, uptime, setUptime, size, setSize, role, setRole]
 
@@ -118,7 +124,7 @@ export default function MainContainer() {
             case 6: return <Screen4 setFirstMech={setFirstMech} timeout={4000} />;
             case 7: return <Output messages={outputMessages[2]} timeout={10000} audio={[`/${hourglassLocation}.wav`]} />;
             case 8: return <Output messages={outputMessages[3]} timeout={10000} audio={[`/platform.wav`]} />;
-            case 9: return <Screen5 setTower={setTower} timeout={10000} />;
+            case 9: return <Screen5 setTower={setTower} timeout={10000} role={role} />;
             case 10: return <Output messages={outputMessages[4]} timeout={6000} />; // after tower
             case 11: return <Output messages={outputMessages[5]} timeout={6000} audio={[`/${instructions[1]}.wav`]} />; // running to first mech
             case 12: return <Output messages={outputMessages[6]} timeout={5000} audio={[`/${instructions[2]}.wav`]} />; // btwn 1st and 2nd
--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -1,25 +1,38 @@
 export default function Preferences({ prefs }) {
     const [prefsOpen, setPrefsOpen, uptime, setUptime, size, setSize, role, setRole] = prefs
 
+    function handleChange(pref, value) {
+        if (pref == "uptime") {
+            setUptime(value)
+            localStorage.setItem("prefs", JSON.stringify({uptime: value, size: size, role: role})) 
+        } else if (pref =="size") {
+            setSize(value)
+            localStorage.setItem("prefs", JSON.stringify({uptime: uptime, size: value, role: role})) 
+        } else if (pref == "role") {
+            setRole(value)
+            localStorage.setItem("prefs", JSON.stringify({uptime: uptime, size: size, role: value})) 
+        } 
+    }
+
     return (
         <div className={prefsOpen ? ("prefs open") : ("prefs")}>
             <div className="pref-group">
                 <p className="pref-name">strat:</p>
-                <button className={uptime ? "settings-button active" : "settings-button"} onClick={() => setUptime(true)}>uptime</button>
-                <button className={uptime ? "settings-button" : "settings-button active"} onClick={() => setUptime(false)}>regular</button>
+                <button className={uptime ? "settings-button active" : "settings-button"} onClick={() => handleChange("uptime", true)}>uptime</button>
+                <button className={uptime ? "settings-button" : "settings-button active"} onClick={() => handleChange("uptime", false)}>regular</button>
             </div>
 
             <div className="pref-group">
                 <p className="pref-name">size:</p>
-                <button className={size == "mini" ? "settings-button active" : "settings-button"} onClick={() => setSize("mini")}>mini</button>
-                <button className={size == "large" ? "settings-button active" : "settings-button"} onClick={() => setSize("large")}>large</button>
+                <button className={size == "mini" ? "settings-button active" : "settings-button"} onClick={() => handleChange("size", "mini")}>mini</button>
+                <button className={size == "large" ? "settings-button active" : "settings-button"} onClick={() => handleChange("size", "large")}>large</button>
             </div>
 
             <div className="pref-group">
                 <p className="pref-name">role:</p>
-                <button className={role == "SGE" ? "settings-button active" : "settings-button"} onClick={() => setRole("SGE")}>SGE</button>
-                <button className={role == "GenericMelee" ? "settings-button active" : "settings-button"} onClick={() => setRole("GenericMelee")}>not SGE (melee)</button>
-                <button className={role == "Generic" ? "settings-button active" : "settings-button"} onClick={() => setRole("Generic")}>not SGE (ranged)</button>
+                <button className={role == "SGE" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "SGE")}>SGE</button>
+                <button className={role == "GenericMelee" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "GenericMelee")}>not SGE (melee)</button>
+                <button className={role == "Generic" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "Generic")}>not SGE (ranged)</button>
             </div>
             
             <button className="close-prefs" onClick={() => setPrefsOpen(false)}>x</button>
