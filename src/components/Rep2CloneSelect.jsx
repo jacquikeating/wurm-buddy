8bb8909a98e8859ead85ff6bbafa290fe4239d7b
jacquikeating <j.keating@live.ca>
2026-03-04 15:24:50 -0500

    Copied content from Screen2 and adapted to suit Rep2CloneSelect

--- src/components/Rep2CloneSelect.jsx
+++ src/components/Rep2CloneSelect.jsx
@@ -1,5 +1,68 @@
-export default function Rep2CloneSelect() {
+import { useContext } from "react"
+import { StepContext } from "../utils/context.js"
+
+export default function Rep2CloneSelect({  }) {
+    const { step, setStep } = useContext(StepContext)
+
+    function handleInput(selectedOption) {
+        // setRep2Clone(selectedOption)
+        console.log(selectedOption)
+        setStep(step + 1)
+    }
+
+    const cloneOptions = [
+        // Has to start with east, not north, because of how the CSS renders the buttons in a circle
+        {
+            location: "E",
+            mechanic: "cone",
+            group: "east"
+        },
+        {
+            location: "SE",
+            mechanic: "def",
+            group: "east"
+        },
+        {
+            location: "S",
+            mechanic: "nothing",
+            group: "west"
+        },
+        {
+            location: "SW",
+            mechanic: "def",
+            group: "west"
+        },
+        {
+            location: "W",
+            mechanic: "cone",
+            group: "west"
+        },
+        {
+            location: "NW",
+            mechanic: "stack",
+            group: "west"
+        },
+        {
+            location: "N",
+            mechanic: "boss",
+            group: "east"
+        },
+        {
+            location: "NE",
+            mechanic: "stack",
+            group: "east"
+        },
+    ]
+
     return (
-        <h1>Rep 2 Clone</h1>
+        <div className="rep-2">
+            {cloneOptions.map((clone, index) => {
+                return (
+                    <button className={"clone-button"} style={{"--i": index}} key={index} onClick={() => handleInput(clone)}>
+                        {clone.location}
+                    </button>
+                )
+            })}
+        </div>
     )
 }
