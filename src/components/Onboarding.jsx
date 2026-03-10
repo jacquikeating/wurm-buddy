e10936e72b4a047a2674b3d8db011b8840b147ec
jacquikeating <j.keating@live.ca>
2026-03-10 12:23:07 -0400

    Added onboarding step 2

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -12,6 +12,18 @@ export default function Onboarding() {
                     <button onClick={() => setOnboardingStep(2)}>start ➜</button>
                 </div>   
             )
+        } else if (onboardingStep == 2) {
+            return (
+                <div className="onboarding">
+                    <div className="header">
+                        <img src="/worm-icon.svg" alt="A red worm" />
+                        <h1>hi, I'm wurm buddy</h1>
+                    </div>
+                    <p>I'm a browser-based tool that can help you with rep 2 and idyllic dream.</p>
+                    <p>I cannot read your game data. Instead, you will tell me which mechanics you get, and I will provide callouts.</p>
+                    <button className="next-btn" onClick={() => setOnboardingStep(3)}>➜</button>
+                </div>   
+            )
         }
     }
 
