d9666745cf314553ec7e8def1f7e213a6a26221e
jacquikeating <j.keating@live.ca>
2026-03-10 12:06:14 -0400

    Added state variable and function to Onboarding to render content in steps

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
+
 export default function Onboarding() {
+    const [onboardingStep, setOnboardingStep] = useState(1)
+
+    function renderContent() {
+        if (onboardingStep == 1) {
+            return (
+                <div className="onboarding">
+                    <img src="/worm-icon.svg" alt="A red worm" />
+                    <h1>wurm buddy</h1>
+                    <button onClick={() => setOnboardingStep(2)}>start ➜</button>
+                </div>   
+            )
+        }
+    }
+
     return (
-        <div className="onboarding">
-            <img src="/worm-icon.svg" alt="A red worm" />
-            <h1>wurm buddy</h1>
-            <button>start ➜</button>
-        </div>    
+        renderContent()   
     )
 }
