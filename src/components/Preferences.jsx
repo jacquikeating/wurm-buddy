2b962ed98af48d7a1c3cd3661020c6c5dd371fca
jacquikeating <j.keating@live.ca>
2026-03-24 23:51:05 -0400

    Preferences renders conditional cotent based on openRolesMenu state

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -13,7 +13,8 @@ export default function Preferences({ prefs }) {
             localStorage.setItem("prefs", JSON.stringify({banana: banana, uptime: uptime, size: value, mechs: mechs, role: role})) 
         } else if (pref == "role") {
             setRole(value)
-            localStorage.setItem("prefs", JSON.stringify({banana: banana, uptime: uptime, size: size, mechs: mechs, role: value})) 
+            localStorage.setItem("prefs", JSON.stringify({banana: banana, uptime: uptime, size: size, mechs: mechs, role: value}))
+            setOpenRolesMenu(false) 
         } else if (pref == "rep2") {
             setBanana(value)
             localStorage.setItem("prefs", JSON.stringify({banana: value, uptime: uptime, size: size, mechs: mechs, role: role})) 
@@ -25,37 +26,11 @@ export default function Preferences({ prefs }) {
 
     return (
         <div className={prefsOpen ? ("prefs open") : ("prefs")}>
-            <div className="pref-group">
-                <p className="pref-name">rep 2:</p>
-                <button className={banana ? "settings-button active" : "settings-button"} onClick={() => handleChange("rep2", true)}>banana</button>
-                <button className={banana ? "settings-button" : "settings-button active"} onClick={() => handleChange("rep2", false)}>clone zone</button>
-            </div>
-            <div className="pref-group">
-                <p className="pref-name">idyllic:</p>
-                <button className={uptime ? "settings-button active" : "settings-button"} onClick={() => handleChange("uptime", true)}>uptime</button>
-                <button className={uptime ? "settings-button" : "settings-button active"} onClick={() => handleChange("uptime", false)}>regular</button>
-            </div>
-
-            <div className="pref-group">
-                <p className="pref-name">size:</p>
-                <button className={size == "mini" ? "settings-button active" : "settings-button"} onClick={() => handleChange("size", "mini")}>mini</button>
-                <button className={size == "large" ? "settings-button active" : "settings-button"} onClick={() => handleChange("size", "large")}>large</button>
-            </div>
-
-            <div className="pref-group">
-                <p className="pref-name">mechs:</p>
-                <button className={mechs == "both" ? "settings-button active" : "settings-button"} onClick={() => handleChange("mechs", "both")}>both</button>
-                <button className={mechs == "rep2" ? "settings-button active" : "settings-button"} onClick={() => handleChange("mechs", "rep2")}>rep 2<br />only</button>
-                <button className={mechs == "idyllic" ? "settings-button active" : "settings-button"} onClick={() => handleChange("mechs", "idyllic")}>idyllic<br />only</button>
-            </div>
-
-            <div className="pref-group">
-                <p className="pref-name">role:</p>
-                    <button className={role == "SGE" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "SGE")}>SGE</button>
-                    <button className="settings-button text-only" onClick={() => setOpenRolesMenu(!openRolesMenu)}>change...</button>
-                    {/* <button className={role == "GenericMelee" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "GenericMelee")}>not SGE<br />(melee)</button> */}
-                    {/* <button className={role == "Generic" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "Generic")}>not SGE<br />(ranged)</button> */}
-                {/* <div className="role-btn-group">
+            {openRolesMenu ? (
+                <div>
+                     <button className={role == "GenericMelee" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "GenericMelee")}>not SGE<br />(melee)</button>
+                    <button className={role == "Generic" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "Generic")}>not SGE<br />(ranged)</button>
+                <div className="role-btn-group">
                     <button className={role == "GenericMelee" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "GenericMelee")}>generic - melee</button>
                     <div className="role-btn-subgroup">
                         <button className={role == "M1" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "M1")}>M1</button>
@@ -65,8 +40,8 @@ export default function Preferences({ prefs }) {
                         <button className={role == "MT" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "MT")}>MT</button>
                         <button className={role == "OT" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "OT")}>OT</button>
                     </div>
-                </div> */}
-                {/* <div className="role-btn-group">
+                </div>
+                <div className="role-btn-group">
                     <button className={role == "Generic" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "Generic")}>generic - ranged</button>
                     <div className="role-btn-subgroup">
                         <button className={role == "R1" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "R1")}>R1</button>
@@ -76,17 +51,47 @@ export default function Preferences({ prefs }) {
                         <button className={role == "H1" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "H1")}>H1</button>
                         <button className={role == "H2" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "H2")}>H2</button>
                     </div>
-                </div> */}
+                </div>
                 
-                {/* <button className={role == "WHM" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "WHM")}>WHM</button>
+                <button className={role == "WHM" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "WHM")}>WHM</button>
                 <button className={role == "AST" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "AST")}>AST</button>
                 <button className={role == "SCH" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "SCH")}>SCH</button>
-                <button className={role == "SGE" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "SGE")}>SGE</button> */}
+                <button className={role == "SGE" ? "settings-button active" : "settings-button"} onClick={() => handleChange("role", "SGE")}>SGE</button>
 
-            </div>
+                </div>
+            ) : (
+                <div>
+                    <div className="pref-group">
+                        <p className="pref-name">rep 2:</p>
+                        <button className={banana ? "settings-button active" : "settings-button"} onClick={() => handleChange("rep2", true)}>banana</button>
+                        <button className={banana ? "settings-button" : "settings-button active"} onClick={() => handleChange("rep2", false)}>clone zone</button>
+                    </div>
+                    <div className="pref-group">
+                        <p className="pref-name">idyllic:</p>
+                        <button className={uptime ? "settings-button active" : "settings-button"} onClick={() => handleChange("uptime", true)}>uptime</button>
+                        <button className={uptime ? "settings-button" : "settings-button active"} onClick={() => handleChange("uptime", false)}>regular</button>
+                    </div>
 
-            <div className={openRolesMenu ? ("roles-menu open") : ("roles-menu")}>Roles Menu</div>
-            
+                    <div className="pref-group">
+                        <p className="pref-name">size:</p>
+                        <button className={size == "mini" ? "settings-button active" : "settings-button"} onClick={() => handleChange("size", "mini")}>mini</button>
+                        <button className={size == "large" ? "settings-button active" : "settings-button"} onClick={() => handleChange("size", "large")}>large</button>
+                    </div>
+
+                    <div className="pref-group">
+                        <p className="pref-name">mechs:</p>
+                        <button className={mechs == "both" ? "settings-button active" : "settings-button"} onClick={() => handleChange("mechs", "both")}>both</button>
+                        <button className={mechs == "rep2" ? "settings-button active" : "settings-button"} onClick={() => handleChange("mechs", "rep2")}>rep 2<br />only</button>
+                        <button className={mechs == "idyllic" ? "settings-button active" : "settings-button"} onClick={() => handleChange("mechs", "idyllic")}>idyllic<br />only</button>
+                    </div>
+
+                    <div className="pref-group">
+                        <p className="pref-name">role:</p>
+                            <button className="settings-button active">{role}</button>
+                            <button className="settings-button text-only" onClick={() => setOpenRolesMenu(true)}>change...</button>
+                    </div>
+                </div>
+            )}            
             <button className="close-prefs" onClick={() => setPrefsOpen(false)}>x</button>
         </div>
     )
