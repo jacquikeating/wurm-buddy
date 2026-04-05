495b0dca99fc47ac4a49eb900645147aa5706906
jacquikeating <j.keating@live.ca>
2026-04-04 22:24:39 -0400

    Imported context, useEffect, handleInput to BloodMana

--- src/components/BloodMana.jsx
+++ src/components/BloodMana.jsx
@@ -1,4 +1,27 @@
-export default function BloodMana() {
+import { useContext, useEffect, useState } from "react"
+import { StepContext } from "../utils/context.js"
+import AudioPlayer from "./AudioPlayer.jsx"
+
+
+export default function BloodMana({ /* setterFunction, timeout */ }) {
+    const { step, setStep } = useContext(StepContext)
+    const [timesUp, setTimesUp] = useState(false)
+
+    // useEffect(() => {
+    //     const reminderTimer = setTimeout(() => {
+    //         setTimesUp(true)
+    //     }, timeout)
+
+    //     return () => {
+    //         clearTimeout(reminderTimer)
+    //     }
+    // }, [])
+
+    function handleInput(selectedOption) {
+        // setterFunction(selectedOption)
+        setStep(step + 1)
+    }
+
     return (
         <h1>Blood Mana</h1>
     )
